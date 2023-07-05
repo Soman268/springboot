e47396a74802dfbbea910526f6b3efb2d5ecc4eb
Soman Kumar <54895319+Soman268@users.noreply.github.com>
2023-07-05 23:11:46 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello Page2!";
+        return "Hello Page20000!";
     }
 
 }
